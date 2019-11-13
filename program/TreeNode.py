acf3de6f963cb79a49f4142000f7f39364122592
xliu <shawliu0315@gmail.com>
2019-11-13 12:18:36 -0330

    init

---
+++ program/TreeNode.py
@@ -0,0 +1,15 @@
+class Node:
+    
+    def __init__(self):
+       self.node_type = 'L'
+       self.parent = None
+       self.pointers=['nil','nil']
+       self.node_page = None
+       self.keys=[]
+
+
+
+
+
+
+
